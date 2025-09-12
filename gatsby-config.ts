ae5ef1c925b83214ec0f7752b27d9e40baae4fff
Rafis Bagautdinov <rafisbb@gmail.com>
2025-09-12 20:29:03 +0600

    fix: remove unused filesystem source configurations for blog and pages

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -11,20 +11,6 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `blog`,
-        path: `${__dirname}/content/blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `${__dirname}/content/pages`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
